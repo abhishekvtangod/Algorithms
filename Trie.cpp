d20554fb0d71dd011d2f27dd3ae4c6ed91461ad7
abhishekvtangod <abhishekvtangod@gmail.com>
2020-04-27 19:19:53 +0530

    april end

--- Trie.cpp
+++ Trie.cpp
@@ -1,6 +1,6 @@
 /**
  *    author:  abhishekvtangod
- *    created:        
+ *    created: 16.04.2020 22:04:59 IST    
 **/
 
 #include<bits/stdc++.h>
@@ -32,11 +32,48 @@ template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order
 //   }
 // };
 
+//trie is matrix representation of trie data structure
 
+//trie[k] represents list of links for the k-th node
+//trie[k][m], node which represents son of k-th node using m-th character
+ll tr[1000006][28];
+
+ll root = 0;
+
+void init()
+{
+	memset(tr,-1,sizeof(tr));
+}
+ll nxt = 1;
+
+void insert(string s)
+{
+	ll node = root;
+	
+	for(ll i=0; i<s.size();i++)
+	{
+		ll q = s[i]-'a';
+		
+		if(tr[node][q] == -1)
+		{
+			tr[node][q] = nxt++;
+		}
+		
+		node = tr[node][q];
+	}
+}
 
 
 void solve()
 {
+	init();
+	ll n,q;cin>>n>>q;
+	for(ll i=0;i<n;i++)
+	{
+		string s;cin>>s;
+		insert(s);
+	}
+	
 	
 }
 
@@ -46,11 +83,11 @@ int main()
     ios::sync_with_stdio(false); 
     cin.tie(nullptr);
 
-	ll t;cin>>t;
-	while(t--)
-	{
+	//ll t;cin>>t;
+	//while(t--)
+	//{
 		solve();
-	}
+	//}
 	
 
     return 0;
